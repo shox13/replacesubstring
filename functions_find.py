f6c424a2a4ff2d6ae1d5767b7b6de5e80aa9b44d
shox13 <shox13@gmail.com>
2014-07-19 21:34:27 -0400

    Streamlining the code

--- functions_find.py
+++ functions_find.py
@@ -1,12 +1,19 @@
 def find(ori_string,old_sub,new_sub):
+  
   count=0
-  for number in range(len(old_sub),len(ori_string)):
-    chunk_start=len(ori_string)-number
-    print chunk_start
-    chunk=ori_string[chunk_start:len(ori_string)-1]
+  
+  for number in range (len(old_sub)-1,len(ori_string)):#for loop runs from 0 index to the remaining length of the string.
+  
+    chunk_start=number
+  #  print chunk_start
+    inch=len(old_sub)-chunk_start# should be the length of old_sub
+    chunk=ori_string[chunk_start:inch]
+  
     if chunk==old_sub:
-      count=count+1
-      ori_string[chunk_start]=new_sub
-    else:
+        count=count+1
+        print count
+        new_sub=ori_string[chunk_start]
+  
+  else:
        print"blah"
   
