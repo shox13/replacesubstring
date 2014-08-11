b1f2b5b18ff449482b6b4bb68f619a709172cf4b
shox13 <shox13@gmail.com>
2014-08-11 12:52:12 -0400

    Final version of code
    Try it!

--- functions_find.py
+++ functions_find.py
@@ -2,13 +2,13 @@ def find(ori_string,old_sub,new_sub):
   #ori_string = original string
   #old_sub = old substring
   #new_sub = new substring
-  if len(ori_string)>len(old_sub) or len(new_sub):
-    for number in range (0,len(ori_string)):
-        chunk=ori_string[number:len(old_sub)+number]
-        if chunk is old_sub:
-            new_sub=ori_string[number]
-        else:
-            print"blah"
+    if len(ori_string)>len(old_sub) or len(new_sub):
+        for number in range (0,len(ori_string)):
+            chunk=ori_string[number:len(old_sub)+number]
+            if chunk is old_sub:
+                ori_string=ori_string[0:number]+new_sub+ori_string[len(old_sub)+number:len(ori_string)]
+            else:
+                a=0
         print ori_string
-  else:
-      print "Not going to work!"
+    else:
+        print "Not going to work!"
